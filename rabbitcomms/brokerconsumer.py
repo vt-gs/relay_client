30219151d127ad7bc321de94b0e80ad82887a0ee
ksterne <kevintyler@vt.edu>
2018-09-27 14:00:44 -0400

    Adding a note on documentation that stop_cons* will keep connection alive

--- rabbitcomms/brokerconsumer.py
+++ rabbitcomms/brokerconsumer.py
@@ -155,6 +155,12 @@ class BrokerConsumer(BrokerConnector):
         messages.  This will send the Basic.Cancel RPC command and makes a clean
         connection break with the broker.  We'll register a callback that will
         be called whne the cancel request is handled.
+
+        IF USING THIS METHOD TO DISCONNECT FROM RMQ SERVER, NOTE THAT THIS
+        WILL RECONNECT TO THE RMQ SERVER AFTER A DELAY OF `retry_wait`.  NOT
+        RECOMMENDED TO USE THIS METHOD TO CHANGE RMQ SERVERS
+
+
         """
         if self.channel:
             if self.loggername is not None:
